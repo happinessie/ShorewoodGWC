bd7aaf83e13889420d61c0eb5230e34a4ea86b18
Martha <Martha.k.dix@gmail.com>
2018-02-13 20:22:56 -0600

    new files
    New files

---
+++ Martha_GWC_Demo/ViewModels/AboutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace Martha_GWC_Demo
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ Martha_GWC_Demo/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Martha_GWC_Demo
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
